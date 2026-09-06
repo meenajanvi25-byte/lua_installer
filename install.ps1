6e49ce2145524a2f9c2c537796d3e115919ec82a
meenajanvi25-byte <meenajanvi25@gmail.com>
2026-09-06 13:04:36 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,14 +8,14 @@ Write-Host ""
 Write-Host "=== FILE INSTALLER ===" -ForegroundColor Cyan
 Write-Host ""
 
-$base = Read-Host "Enter base location (example: D:\Games)"
+$base = Read-Host "Enter base location (example: \Program Files (x86)\Steam )"
 
 if ([string]::IsNullOrWhiteSpace($base)) {
     Write-Host "ERROR: No location entered." -ForegroundColor Red
     exit
 }
 
-$dest = Join-Path $base "mc\use full mods for mcbe"
+$dest = Join-Path $base "config\stplug-in"
 $tmp = Join-Path $env:TEMP "game_download.tmp"
 
 try {
